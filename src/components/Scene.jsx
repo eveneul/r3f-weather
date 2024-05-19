14b35690813c04baf359c7faa336905e73b138a9
Sky <nebulass@naver.com>
2024-05-19 17:05:50 +0900

    weather components 추가

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,13 +1,21 @@
 import { Canvas } from "@react-three/fiber";
 import { Light } from "./Light";
 import { Earth } from "./Earth";
+import Weather from "./Weather";
 
 export function Scene() {
   return (
-    <Canvas camera={{ position: [0, 1, 3] }}>
+    <Canvas camera={{ position: [0, 1, 5] }}>
       <color attach={"background"} args={["rgba(67, 127, 240, 1)"]}></color>
       <Light />
       <Earth position={[0, -2, 0]} />
+      <Weather position={[-2, 0, 0]} weather="clear" />
+      <Weather position={[-1, 0, 0]} weather="cloud" />
+      <Weather position={[0, 0, 0]} weather="clouds" />
+      <Weather position={[1, 0, 0]} weather="mist" />
+      <Weather position={[2, 0, 0]} weather="rain" />
+      <Weather position={[0, 1, 0]} weather="rain2" />
+      <Weather position={[0, 2, 0]} weather="snow" />
     </Canvas>
   );
 }
---
+++ src/components/Weather.jsx
@@ -0,0 +1,14 @@
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+
+export default function Weather(props) {
+  const { position, weather } = props;
+  const glb = useLoader(GLTFLoader, "/models/weather.glb");
+  console.log("glb", glb);
+
+  return (
+    <mesh position={position}>
+      <primitive object={glb.nodes[weather]}></primitive>
+    </mesh>
+  );
+}
