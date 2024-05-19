14e0822b3a8bfcf3147d5c77ab9e1cba04ceabf8
Sky <nebulass@naver.com>
2024-05-19 21:22:18 +0900

    모델링과 api 연동

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -21,30 +21,37 @@ export function Scene() {
 
   useEffect(() => {
     getCitiesWeather();
-    // const fetchData = async () => {
-    //   const data = await getCityWeather("Seoul", key);
-    //   console.log(data);
-    // };
-
-    // fetchData();
   }, []);
-
-  useEffect(() => {
-    console.log(cityContent, "cityContent"), [cityContent];
-  });
 
   return (
     <Canvas camera={{ position: [0, 1, 5] }}>
       <color attach={"background"} args={["rgba(67, 127, 240, 1)"]}></color>
       <Light />
       <Earth position={[0, -2, 0]} />
-      <Weather position={[-2, 0, 0]} weather="clear" />
-      <Weather position={[-1, 0, 0]} weather="cloud" />
-      <Weather position={[0, 0, 0]} weather="clouds" />
-      <Weather position={[1, 0, 0]} weather="mist" />
-      <Weather position={[2, 0, 0]} weather="rain" />
-      <Weather position={[0, 1, 0]} weather="rain2" />
-      <Weather position={[0, 2, 0]} weather="snow" />
+      {cityContent && (
+        <>
+          <Weather
+            position={[-2, 0, 0]}
+            weather={cityContent[0].weather.weather[0].main.toLowerCase()}
+          />
+          <Weather
+            position={[-1, 0, 0]}
+            weather={cityContent[1].weather.weather[0].main.toLowerCase()}
+          />
+          <Weather
+            position={[0, 0, 0]}
+            weather={cityContent[2].weather.weather[0].main.toLowerCase()}
+          />
+          <Weather
+            position={[1, 0, 0]}
+            weather={cityContent[3].weather.weather[0].main.toLowerCase()}
+          />
+          <Weather
+            position={[2, 0, 0]}
+            weather={cityContent[4].weather.weather[0].main.toLowerCase()}
+          />
+        </>
+      )}
     </Canvas>
   );
 }
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,9 +4,18 @@ import { GLTFLoader } from "three/examples/jsm/Addons.js";
 export default function Weather(props) {
   const { position, weather } = props;
   const glb = useLoader(GLTFLoader, "/models/weather.glb");
+
+  let weatherModel;
+
+  if (glb.nodes[weather]) {
+    weatherModel = glb.nodes[weather].clone();
+  } else {
+    weatherModel = glb.nodes.cloud.clone();
+  }
+
   return (
     <mesh position={position}>
-      <primitive object={glb.nodes[weather]}></primitive>
+      <primitive object={weatherModel}></primitive>
     </mesh>
   );
 }
