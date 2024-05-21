b64b1cd6886e20552ce8a37595903375c3924c72
Sky <nebulass@naver.com>
2024-05-21 21:01:52 +0900

    환경맵 추가

--- src/components/Light.jsx
+++ src/components/Light.jsx
@@ -1,6 +1,9 @@
+import { Environment } from "@react-three/drei";
+
 const Light = () => {
   return (
     <>
+      <Environment preset="forest" />
       <directionalLight position={[1, 3, -1]} intensity={3} />
       <ambientLight intensity={1} />
     </>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,7 +16,7 @@ export function Home() {
   return (
     <>
       <Canvas camera={{ position: [0, 1, 5] }}>
-        <color attach={"background"} args={["rgba(67, 127, 240, 1)"]}></color>
+        {/* <color attach={"background"} args={["rgba(67, 127, 240, 1)"]}></color> */}
         <Suspense fallback={<Sphere />}>
           {/* <Sphere /> */}
           <Scene />
